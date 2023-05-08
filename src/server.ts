64c6e7665f62a4428fe4cf40afac0d297b4d637e
marcoscl84 <marcoscontelima@yahoo.com.br>
2023-05-08 15:49:16 -0300

    feat: cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
 
 const app = Fastify();
 const prisma = new PrismaClient();
+
+app.register(cors, {
+  // registra as rotas do front-end que podem acessar o back-end
+  origin: ["http://localhost:3333/"],
+});
 
 app.get("/hello", async () => {
   const habits = await prisma.habit.findMany({
