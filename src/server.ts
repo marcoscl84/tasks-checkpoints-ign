83e708d394d319201bbee88807e2653d9d2d331c
marcoscl84 <marcoscontelima@yahoo.com.br>
2023-05-08 15:13:15 -0300

    feat: Configuração inicial + prisma

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.get("/hello", async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "beber",
+      },
+    },
+  });
+
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server is running my bro!");
+  });
