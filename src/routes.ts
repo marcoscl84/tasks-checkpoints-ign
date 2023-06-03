534c74b542e0ad713472eb62fe2d8c68e9912168
marcoscl84 <marcoscontelima@yahoo.com.br>
2023-06-03 20:56:51 -0300

    feat: rota de criação de novo hábito

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,32 @@
+import dayjs from "dayjs";
 import { prisma } from "./lib/prisma";
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const habits = await prisma.habit.findMany({
-      where: {
-        title: {
-          startsWith: "beber",
+  app.post("/habits", async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)), // dias de domingo a sábado
+    });
+
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    // retorna data atual com horas, minutos e segundos zerados
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((weekday) => {
+            return {
+              week_day: weekday,
+            };
+          }),
         },
       },
     });
-
-    return habits;
   });
 }
