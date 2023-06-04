ee76efae05fe9c241fcc8b521e710cc09913aefa
marcoscl84 <marcoscontelima@yahoo.com.br>
2023-06-04 10:05:50 -0300

    feat: rota get de hábitos completos e possíveis

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log: ["query"], // mostra no console todas as queries feitas no banco
+});
--- src/routes.ts
+++ src/routes.ts
@@ -29,4 +29,48 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
   });
+
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(), // coerce converte o parametro de string para date
+    });
+
+    const { date } = getDayParams.parse(request.query);
+
+    const parseDate = dayjs(date).startOf("day");
+    const weekDay = parseDate.get("day");
+
+    console.log(date, weekDay);
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date, // menor ou igual a data atual
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parseDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
+  });
 }
